fcf51302310b8a20d57da255fc2f34a5799dd60a
Andres Cajamarca <afcajamarcar@unal.edu.co>
2024-03-31 15:51:52 -0400

    briefly style body

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,25 @@
 'use client'
 import Header from './components/Header'
+import Footer from './components/Footer'
 
 export default function Home() {
   return (
     <>
       <Header/>
-      <main className="flex h-screen">
-        <section className='flex justify-center'>
+      <main className="block h-screen m-10">
+        <section className='flex flex-col text-center'>
           <h1>Andres Cajamarca</h1>
           <h2>Senior Software Engineer</h2>
         </section>
+        <section className='grid grid-cols-2 p-5'>
+          <p>CARD</p>
+          <p>CARD</p>
+          <p>CARD</p>
+          <p>CARD</p>
+          <p>CARD</p>
+        </section>
       </main>
+      <Footer />
     </>
   )
 }
