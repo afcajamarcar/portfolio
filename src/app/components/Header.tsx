3448aaef0dc86710d4c4940cce4d69e240f0e046
Andres Cajamarca <afcajamarcar@unal.edu.co>
2024-03-30 20:33:09 -0400

    add responsiveness to header component

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,25 +1,35 @@
 import React from 'react'
 import Button from './Button'
 
-const Header = () => (
-  <header className='sticky top-0 z-50 p-5 bg-slate-900'>
+const Header = () => {
+  const renderNav = () => (
     <nav className='flex justify-end'>
       <ol className='flex flex-row space-x-8'>
         <li>
-          <Button text='Experience' href='#experience'/>
+          <Button text='Experience' href='#experience' />
         </li>
         <li>
-        <Button text='CV' href='#cv' />
+          <Button text='CV' href='#cv' />
         </li>
         <li>
           <Button text='Projects' href='#projects' />
         </li>
         <li>
-        <Button text='Contact' href='#contact' />
+          <Button text='Contact' href='#contact' />
         </li>
       </ol>
     </nav>
-  </header>
-)
+  )
+  return (
+    <header className='sticky top-0 z-50 p-5 bg-slate-900'>
+      <div className='hidden md:block'>
+        {renderNav()}
+      </div>
+      <div className='block md:hidden'>
+        <h1>Hamburguer menu here</h1>
+      </div>
+    </header>
+  )
+}
 
 export default Header
