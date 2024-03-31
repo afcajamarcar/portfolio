20600fb8484b45ff4aba26f979db95561f35f090
Andres Cajamarca <afcajamarcar@unal.edu.co>
2024-03-31 15:17:43 -0400

    add headlessui components, add popover to hamburguer menu in mobile design

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react'
-import Button from './Button'
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { Popover, Transition } from '@headlessui/react'
+import Button from './Button'
 
 const Header = () => {
-  const renderNav = () => (
-    <nav className='flex justify-end'>
-      <ol className='flex flex-row space-x-8'>
+  const renderNav = (navClassName: string, olClassName: string): JSX.Element => (
+    <nav className={navClassName}>
+      <ol className={olClassName}>
         <li>
           <Button text='Experience' href='#experience' />
         </li>
@@ -23,13 +23,30 @@ const Header = () => {
       </ol>
     </nav>
   )
+
   return (
     <header className='sticky top-0 z-50 p-5 bg-slate-900'>
       <div className='hidden md:block'>
-        {renderNav()}
+        {renderNav('flex justify-end', 'flex flex-row space-x-8')}
       </div>
       <div className='block md:hidden'>
-        <FontAwesomeIcon icon={faBars} />
+        <Popover>
+          <Popover.Button>
+            <FontAwesomeIcon icon={faBars} />
+          </Popover.Button>
+          <Transition
+            enter="transition duration-100 ease-out"
+            enterFrom="transform scale-95 opacity-0"
+            enterTo="transform scale-100 opacity-100"
+            leave="transition duration-75 ease-out"
+            leaveFrom="transform scale-100 opacity-100"
+            leaveTo="transform scale-95 opacity-0"
+          >
+            <Popover.Panel className="absolute z-10 w-screen h-screen bg-black">
+              {renderNav('', '')}
+            </Popover.Panel>
+          </Transition>
+        </Popover>
       </div>
     </header>
   )
