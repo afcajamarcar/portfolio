d290041c8478a87aa34ae5d74080be9811487eaf
Andres Cajamarca <afcajamarcar@unal.edu.co>
2024-03-31 15:30:44 -0400

    add initial version of footer

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <footer className='flex flex-col text-center p-5 bg-slate-900'>
+      <p>
+        Made with NextJS, TypeScript and Tailwind
+      </p>
+      <p>
+        2024
+      </p>
+    </footer>
+  )
+}
+
+export default Footer
