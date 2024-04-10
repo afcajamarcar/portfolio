6a25febb758d8b5a4a592a61b5c5d93581f18763
Andres Cajamarca <afcajamarcar@unal.edu.co>
2024-04-10 19:08:31 -0400

    fix identation, add sections for interests and socials

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 'use client'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faGithub, faLinkedin, faInstagram } from '@fortawesome/free-brands-svg-icons'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Card from './components/Card'
 
+
 export default function Home() {
   return (
     <>
-      <Header/>
+      <Header />
       <main className="block h-screen m-10">
         <section className='flex flex-col text-center'>
           <h1>Andres Cajamarca</h1>
@@ -18,24 +21,47 @@ export default function Home() {
             Vivamus varius dapibus nisi, non condimentum elit semper vel. Proin non enim ultrices, rutrum sem quis, hendrerit erat.
           </p>
         </section>
-        <h1>
-          Experience
-        </h1>
-        <section className='grid grid-cols-2 p-5'>
-          <Card
-            title='Frontend Developer'
-            subtitle='“Mercado Libre” (e-commerce)'
-            upperNote='MM/YY - Present'
-            note='Bogotá, Colombia'
-            items={[
-              'first item',
-              'second item',
-            ]}
-            badges={[
-              { text: 'React' },
-              { text: 'Java' },
-            ]}
-          />
+        <section className='grid grid-cols-2 p-5 gap-8'>
+          <section className='flex flex-col space-y-4'>
+            <h1>
+              Experience
+            </h1>
+            <Card
+              title='Frontend Developer'
+              subtitle='“Mercado Libre” (e-commerce)'
+              upperNote='MM/YY - Present'
+              note='Bogotá, Colombia'
+              items={[
+                'first item',
+                'second item',
+              ]}
+              badges={[
+                { text: 'React' },
+                { text: 'Java' },
+              ]}
+            />
+          </section>
+          <section className='flex flex-col space-y-8'>
+            <section className='flex flex-col space-y-4'>
+              <h1>Pojects</h1>
+              <p>Space to insert projects cards...</p>
+            </section>
+            <section className='flex flex-col space-y-4'>
+              <h1>Interests</h1>
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                Maecenas pellentesque purus urna, mattis bibendum enim congue vel.
+              </p>
+            </section>
+            <section className='flex flex-col space-y-4'>
+              <h1>Socials</h1>
+              <div className='flex flex-row gap-5'>
+                <FontAwesomeIcon size='4x' icon={faGithub} />
+                <FontAwesomeIcon size='4x' icon={faLinkedin} />
+                <FontAwesomeIcon size='4x' icon={faInstagram} />
+              </div>
+            </section>
+          </section>
         </section>
       </main>
       <Footer />
